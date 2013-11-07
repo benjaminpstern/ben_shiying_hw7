9d5f54c03908149145b8462307bde60e964a659f
SHIYING <szheng@oberlin.edu>
2013-11-06 22:01:32 -0500

    Finished mystrtol in sort.c

--- sort.c
+++ sort.c
@@ -8,17 +8,55 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
+#include <ctype.h>
+#define LINE_LENGTH 1024
 
 int reverse=0;
 
-char* read_lines(){
-	//TODO to be implemented
+char** read_lines(){
+	//TODO
+	//read the first 1024 characters of each line 
+	//add them into an array of strings
+	//until seeing EOF, and then return the array
 	return NULL;
 }
 
 long mystrtol(char *start, char **rest){
-	//TODO to be implemented
-	return 0;
+	int exit=0;
+	long number=0;
+	int sign=1;
+	int counter=0;
+	char begin[LINE_LENGTH+1];
+	strcpy(begin,start);
+	*rest=start;
+	while(!exit){
+		if (begin[counter]==' '){
+			counter++;
+		}
+		else{
+			exit=1;
+		}
+	}
+	if (begin[counter]=='+'){
+		counter++;
+	}
+	else if (begin[counter]=='-'){
+		sign=-1;
+		counter++;
+	}
+	exit=0;
+	while(!exit){
+		if (!isdigit(begin[counter])){
+			exit=1;
+			*rest=&start[counter];
+			number=number*sign;
+		}
+		else{
+			number=number*10+(begin[counter]-'0');
+			counter++;
+		}
+	}
+	return number;
 }
 
 int num_cmp (const void * a, const void * b){
@@ -36,16 +74,20 @@ void string_sort(int case_sensitive){
 }
 
 int main(){
-	long* a=malloc(sizeof(long));
+  /*	long* a=malloc(sizeof(long));
 	long* b=malloc(sizeof(long));
 	*a = 5;
 	*b = 6;
-	printf("%d", num_cmp(a,b));
-/*	long a;
-	long b;
-	a=5;
-	b=6;
-	long* x = &a;
-	long* y = &b;
-	printf("%d",num_cmp(x,y));*/
+	printf("%d", num_cmp(a,b));*/
+	char* s = malloc(sizeof(char)*5);
+	char* rest;
+	long num;
+	s[0]='4';
+	s[1]='5';
+	s[2]='c';
+	s[3]='d';
+	s[4]=0;
+	num=mystrtol(s,&rest);
+	printf("%ld, %s", num, rest);
+	free(s);
 }
